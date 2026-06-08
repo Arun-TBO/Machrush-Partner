27ccf9d615aebb8544245d65d5467d29a111d8a8
harishanmugaprasad <harishanmugaprasad@thebrandopedia.in>
2026-06-08 15:50:44 +0530

    complete the resloved

--- app/payment-pending.tsx
+++ app/payment-pending.tsx
@@ -423,11 +423,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff',
-<<<<<<< HEAD
     padding : rs(10),
-=======
-     padding : 18
->>>>>>> 65b6a4c6fe790f9c0668b4549f9dacdb5ae9df8d
   },
   header: {
     position: 'absolute',
@@ -486,13 +482,9 @@ const styles = StyleSheet.create({
   },
   summaryBlock: {
     width: '100%',
-<<<<<<< HEAD
     gap: vs(16),
     paddingHorizontal: rs(16),
     paddingVertical: vs(24),
-=======
-    gap: 16,
->>>>>>> 65b6a4c6fe790f9c0668b4549f9dacdb5ae9df8d
   },
   deliveryHeaderRow: {
     width: '100%',
--- app/payment-received.tsx
+++ app/payment-received.tsx
@@ -501,17 +501,9 @@ const styles = StyleSheet.create({
   },
   summaryBlock: {
     width: '100%',
-<<<<<<< HEAD
     gap: vs(16),
     paddingHorizontal: rs(16),
     paddingVertical: vs(24),
-=======
-    gap: 16,
-    
-<<<<<<< HEAD
->>>>>>> 671bddb (complete the alignment)
-=======
->>>>>>> 65b6a4c6fe790f9c0668b4549f9dacdb5ae9df8d
   },
   deliveryHeaderRow: {
     width: '100%',
